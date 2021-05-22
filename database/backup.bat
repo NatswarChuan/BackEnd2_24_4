9d8e23be1d78de5df72d6a9baea434f30180badc
NatswarChuan <natswarchuan@gmail.com>
2021-05-22 07:33:26 +0700

    upload

---
+++ database/backup.bat
@@ -0,0 +1,9 @@
+  
+@echo off
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set path=C:\wamp64\bin\mariadb\mariadb10.4.10\bin
+mysqldump -uroot -p laravel> laravel_%datestamp%_%timestamp%.sql
